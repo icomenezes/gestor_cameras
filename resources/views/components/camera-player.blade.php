53b68d04cb42bc3c72f4af6f39fc99c5d9ec2f4a
icomenezes <icomenezes@hotmail.com>
2026-05-23 10:52:06 -0300

    teste

--- resources/views/components/camera-player.blade.php
+++ resources/views/components/camera-player.blade.php
@@ -11,11 +11,10 @@
 
 <div class="relative w-full h-full bg-black flex items-center justify-center" id="{{ $playerId }}-wrap">
 
-    {{-- WebRTC (go2rtc via proxy Laravel) --}}
+    {{-- WebRTC direto (URL já vem configurada via GO2RTC_PUBLIC_URL) --}}
     @if($isWebRTC)
         @php
-            preg_match('/[?&]src=([^&]+)/', $url, $m);
-            $proxyUrl = route('go2rtc.webrtc') . '?src=' . ($m[1] ?? '');
+            $proxyUrl = $url;
         @endphp
         <video id="{{ $playerId }}" class="w-full h-full" autoplay muted playsinline controls></video>
         <div id="{{ $playerId }}-status" class="absolute inset-0 flex flex-col items-center justify-center bg-black/80 text-gray-400 text-sm gap-2">
